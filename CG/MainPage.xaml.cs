498643684954948dc8e10b78329abe2d3f166d42
Ilya <voloshin.ilya.andreevich@gmail.com>
2021-03-17 18:45:08 +0200

    initial commit

---
+++ CG/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CG
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        private async void RegistrBtnClick(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegistrPage());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("In Development","Coming soon","Close");
+        }
+    }
+}
