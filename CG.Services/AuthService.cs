6588aa957633b52c3a63547012dcaec0f72d8204
ilyasamuray <voloshin.ilya.andreevich@gmail.com>
2021-03-20 18:04:22 +0200

    Add way to get jwt

---
+++ CG.Services/AuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace CG.Services
+{
+    public class AuthService
+    {
+        public void GetJwtToken()
+        {
+            var httpClient = new HttpClient();
+        }
+    }
+}
